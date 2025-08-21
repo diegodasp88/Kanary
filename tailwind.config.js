65c4ad32fb913c7359681228fd229cdab35b1375
DevSamuelJ <samuel701silva@gmail.com>
2025-08-20 23:48:01 -0300

    Iniciando a SideBar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'meu-azul': '#1fb6ff',
+        'meu-roxo': '#7e5bef',
+        // ...adicione outras cores aqui
+      },
+    },
+  },
+  // ...outras configurações
+}
